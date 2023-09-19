af97e24aea5905066332d7fc0df56908ec17b74d
Matheus Dall olmo <102554207+matheusdallolmo@users.noreply.github.com>
2023-09-18 21:49:49 -0300

    Add files via upload

--- JogoGeneral.java
+++ JogoGeneral.java
@@ -1,6 +1,6 @@
 public class JogoGeneral {
     private Dado[] dados = new Dado[5];
-    private int[] jogadas = new int[13]; 
+    private int[] jogadas = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}; 
 
     public void rolarDados(){
         for(int i=0; i<5; i++){
@@ -8,80 +8,97 @@ public class JogoGeneral {
         }
     }
 
-    public void validarJogada(int x){
-        int i;
-        int soma = 0;
-        switch (x):
-            case 1:
-                for(i=0; i<6; i++){
-                    if (dados[i] == 1)
-                        soma++;
-                }
-                jogadas[0] = soma;
-            break;
-            case 2:
-                for(i=0; i<6; i++){
-                    if (dados[i] == 2)
-                        soma += dados[i];
-                }
-                jogadas[1] = soma;
-            break;
-            case 3:
-                for(i=0; i<6; i++){
-                    if (dados[i] == 3)
-                        soma += dados[i];
-                }
-                jogadas[2] = soma;
-            break;
-            case 4:
-                for(i=0; i<6; i++){
-                    if (dados[i] == 4)
-                        soma += dados[i];
-                }
-                jogadas[3] = soma;
-            break;
-            case 5:
-                for(i=0; i<6; i++){
-                    if (dados[i] == 5)
-                        soma += dados[i];
-                }
-                jogadas[4] = soma;
-            break;
-            case 6:
-                for(i=0; i<6; i++){
-                    if (dados[i] == 6)
-                        soma += dados[i];
-                }
-                jogadas[5] = soma;
-            break;
+    // Funcao que valida se a jogada e possivel e calcula os pontos marcados
+    // Retorna 0 se a jogada nao for possivel ou os pontos marcados se ela for possivel
+    public int validarJogada(int jogada){
+        int i, quant = 0;
+        // Verifica e calcula os pontos para a Jogada de 1's
+        if(jogada == 1){
+            for(i = 0; i < 5; i++)
+                if(dados[i].getSideUp() == 1)
+                    quant += 1;
+            return quant;
+        }
+        // Verifica e calcula os pontos para a Jogada de 2's
+        else if(jogada == 2){
+            for(i = 0; i < 5; i++)
+                if(dados[i].getSideUp() == 2)
+                    quant += 2;
+            return quant;
+        }
+        // Verifica e calcula os pontos para a Jogada de 3's
+        else if(jogada == 3){
+            for(i = 0; i < 5; i++)
+                if(dados[i].getSideUp() == 3)
+                    quant += 3;
+            return quant;
+        }
+        // Verifica e calcula os pontos para a Jogada de 4's
+        else if(jogada == 4){
+            for(i = 0; i < 5; i++)
+                if(dados[i].getSideUp() == 4)
+                    quant += 4;
+            return quant;
+        }
+        // Verifica e calcula os pontos para a Jogada de 5's
+        else if(jogada == 5){
+            for(i = 0; i < 5; i++)
+                if(dados[i].getSideUp() == 5)
+                    quant += 5;
+            return quant;
+        }
+        // Verifica e calcula os pontos para a Jogada de 6's
+        else if(jogada == 6){
+            for(i = 0; i < 5; i++)
+                if(dados[i].getSideUp() == 6)
+                    quant += 6;
+            return quant;
+        }
+        // Verifica e calcula os pontos para a Trinca(T)
+        else if(jogada == 7){
+            for(i = 0; i < 5; i++){
+                quant = 0;
+                for(int j = 0; j < 5; j++)
+                    if(dados[i].getSideUp() == dados[j].getSideUp()){
+                        quant += 1;
+                        if(quant >= 3){
+                            quant = 0;
+                            for(int k = 0; k < 5; k++)
+                                quant += dados[k].getSideUp();
+                            return quant;
+                        }
+                            
+                    }
+            }
+            return 0;
+        }
+        // Verifica e calcula os pontos para a Quadra(Q)
+        else if(jogada == 7){
+            for(i = 0; i < 5; i++){
+                quant = 0;
+                for(int j = 0; j < 5; j++)
+                    if(dados[i].getSideUp() == dados[j].getSideUp()){
+                        quant += 1;
+                        if(quant >= 4){
+                            for(int k = 0; k < 5; k++)
+                                if(dados[i].getSideUp() == dados[k].getSideUp())
+                                    quant += dados[i].getSideUp();
+                            return quant;
+                        }
+                            
+                    }
+            }
+            return 0;
+        }
+        // Verifica e calcula os pontos para Full-Hand(F)/Full-House(F)
+        else if(jogada == 8){
+            
+        }
 
-            case 7:
-                int[] cont = new int[6];
-
-                for(i=0; i<6; i++){
-                    if (dados[i] == 1)
-                        cont[0]++;
-                }
-            break;
-            case 8:
-                
-            break;
-            case 9:
-                
-            break;
-            case 10:
-                
-            break;
-            case 11:
-                
-            break;
-            case 12:
-                
-            break;
-            case 13:
-                
-            break;
-
+        return -1;
     }
 
+    public void pontuarJogada(int jogada, int pontos){
+        jogadas[jogada - 1] = pontos;
+    }
 }
