46aad3ff17ceadecfdd5e016af8fc3ba6ba2f10b
Matheus Dall olmo <102554207+matheusdallolmo@users.noreply.github.com>
2023-09-20 15:35:05 -0300

    Add Jogador.java

---
+++ Jogador.java
@@ -0,0 +1,35 @@
+public class Jogador {
+    private String nome = new String();
+    private char tipoJogador;
+    private JogoGeneral jogo = new JogoGeneral();
+
+    public Jogador(String nome, char tipo){
+        this.nome = nome;
+        tipoJogador = tipo;
+    }
+
+    public void jogarDados(){
+        if(tipoJogador == 'H')
+            System.out.println("Rolando dados para"+nome+" (H)");
+        else
+            System.out.println("Rolando dados para"+nome+" (M)");
+        System.out.println("Valore obtidos: ");
+        jogo.rolarDados();
+    }
+
+    public void escolherJogada(){
+        int aux;
+        if(tipoJogador == 'H'){
+            System.out.println("-> "+nome+", para qual jogada deseja marcar [1 - 13]?");
+            System.out.println("1 2 3 4 5 6 7(T) 8(Q) 9(F) 10(S+) 11(S-) 12(G) 13(X)\n");
+            for(int i = 0; i < 13; i++){
+                aux = jogo.getPontuacao(i);
+                if(aux >= 0)
+                    System.out.print(aux );
+                else
+                    System.out.print("- ");
+            }
+        };
+
+    }
+}
