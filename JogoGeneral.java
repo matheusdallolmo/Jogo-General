20186a8630f180864f3ca5d29fa08d93447c2fdc
Benetti03 <102761285+Benetti03@users.noreply.github.com>
2023-09-20 15:16:58 -0300

    Add files via upload

--- JogoGeneral.java
+++ JogoGeneral.java
@@ -16,7 +16,7 @@ public class JogoGeneral {
         if(jogada == 1){
             for(i = 0; i < 5; i++)
                 if(dados[i].getSideUp() == 1)
-                    quant += 1;
+                    quant += 1;                                                   
             return quant;
         }
         // Verifica e calcula os pontos para a Jogada de 2's
@@ -58,42 +58,103 @@ public class JogoGeneral {
         else if(jogada == 7){
             for(i = 0; i < 5; i++){
                 quant = 0;
-                for(int j = 0; j < 5; j++)
+                for(int j = 0; j < 5; j++){
                     if(dados[i].getSideUp() == dados[j].getSideUp()){
                         quant += 1;
-                        if(quant >= 3){
+                        if(quant >= 3){ //Verifica se existem 3 dados iguais
                             quant = 0;
                             for(int k = 0; k < 5; k++)
-                                quant += dados[k].getSideUp();
+                                quant += dados[k].getSideUp();   //se existirem, retorna a soma dos dados
                             return quant;
                         }
                             
                     }
+                }
             }
             return 0;
         }
+
         // Verifica e calcula os pontos para a Quadra(Q)
-        else if(jogada == 7){
+        else if(jogada == 8){
             for(i = 0; i < 5; i++){
                 quant = 0;
-                for(int j = 0; j < 5; j++)
+                for(int j = 0; j < 5; j++){
                     if(dados[i].getSideUp() == dados[j].getSideUp()){
                         quant += 1;
-                        if(quant >= 4){
+                        if(quant >= 4){  //Verifica se existem 4 dados iguais
+                             quant = 0;
                             for(int k = 0; k < 5; k++)
-                                if(dados[i].getSideUp() == dados[k].getSideUp())
-                                    quant += dados[i].getSideUp();
+                                quant += dados[k].getSideUp();   //se existirem, retorna a soma dos dados
                             return quant;
-                        }
-                            
+                        }  
                     }
+                }
             }
             return 0;
         }
+
         // Verifica e calcula os pontos para Full-Hand(F)/Full-House(F)
-        else if(jogada == 8){
-            
+        else if(jogada == 9){
+            for(i=0; i<5; i++){
+                for(int j=0; j<5; j++){
+                    if(dados[i].getSideUp() != dados[j].getSideUp()){
+                        for(int k =0; k<5; k++){
+                            if((dados[k].getSideUp() != dados[i].getSideUp()) && (dados[k].getSideUp() != dados[j].getSideUp()))  //verifica se ha mais de dois numeros diferentes
+                                return 0;                                              //verifica com os dois possiveis numeros do FH, caso haja mais de um num. diferente, retorna 0
+                        }
+                        return 25;
+                    }
+                }
+            }
         }
+
+        //Verifica e calcula os pontos para Sequencia Alta (S+)
+        else if (jogada == 10){
+            for(i = 0; i<5; i++){
+                for(int j=0; j<5; j++){
+                    if(dados[j].getSideUp() == 1) //na seq. baixa nao pode haver 1, portanto, verifica
+                        return 0;
+                    if(dados[i].getSideUp() == dados[j].getSideUp()) //em ambas as sequencias nao pode haver numeros iguais
+                        return 0;
+                }
+            }
+            return 30;
+        }
+
+        //Verifica e calcula os pontos para Sequencia Baixa (S-)
+        else if (jogada == 11){
+            for(i = 0; i<5; i++){
+                for(int j=0; j<5; j++){
+                    if(dados[j].getSideUp() == 6)   //na seq. baixa nao pode haver 6, portanto, verifica
+                        return 0;
+                    if(dados[i].getSideUp() == dados[j].getSideUp()) //em ambas as sequencias nao pode haver numeros iguais
+                        return 0;
+                }
+            }
+            
+            return 40;
+        }
+
+        //Verifica e calcula os pontos para General(G)
+        else if (jogada == 12){
+            for(i=0; i<5; i++)
+                for(int j=0; j<5; j++)
+                    if(dados[i].getSideUp() != dados[j].getSideUp()) //Caso algum dado for diferente, nao eh general
+                        return 0;
+            
+            return 50;      
+        }
+        
+        //Verifica e calcula os pontos para Jogada Aleatoria(X)
+        else if (jogada == 13){
+            quant = 0;
+            for(i = 0; i<5; i++)
+                quant += dados[i].getSideUp();   //Retorna a soma de todos os dados
+
+            return quant;
+        }
+
+        
 
         return -1;
     }
