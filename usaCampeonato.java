1f7612a26a3103ab9d36aba13f8d2dde8bbeb55e
Benetti03 <102761285+Benetti03@users.noreply.github.com>
2023-10-04 14:20:03 -0300

    Add files via upload

--- usaCampeonato.java
+++ usaCampeonato.java
@@ -4,42 +4,49 @@ public class usaCampeonato{
     public static void main(String[] args){
         Campeonato camp = new Campeonato();
         Scanner scanner = new Scanner(System.in);
-        int opcao;
+        char opcao;
+
         do{
             System.out.println("\n..:: Menu Interativo ::..");
-            System.out.println("1 -> Incluir Jogador");
-            System.out.println("2 -> Remover Jogador");
-            System.out.println("3 -> Iniciar Campeonato");
-            System.out.println("4 -> Mostrar a Cartela de Resultados");
-            System.out.println("5 -> Gravar o Campeonato em arquivo");
-            System.out.println("6 -> Ler Campeonato de arquivo");
-            System.out.println("7 -> Sair");
+            System.out.println("a -> Incluir Jogador");
+            System.out.println("b -> Remover Jogador");
+            System.out.println("c -> Iniciar Campeonato");
+            System.out.println("d -> Mostrar a Cartela de Resultados");
+            System.out.println("e -> Gravar o Campeonato em arquivo");
+            System.out.println("f -> Ler Campeonato de arquivo");
+            System.out.println("g -> Sair\n");
 
-            opcao = scanner.nextInt();
+            System.out.println("Escolha uma opcao: ");
+            opcao = scanner.next().charAt(0);
 
             switch(opcao){
-                case 1:
+                case 'a':
                     camp.incluirJogador();
                 break;
-                case 2:
+                case 'b':
                     camp.removerJogador();
                 break;
-                case 3:
+                case 'c':
                     camp.iniciarCampeonato();
                 break;
-                case 4:
+                case 'd':
                     camp.mostrarTabela();
                 break;
-                case 5:
+                case 'e':
                     camp.gravarEmArquivo();
                 break;
-                case 6:
+                case 'f':
                     camp.lerDoArquivo();
                 break;
+                case 'g':
+                break;
+
                 default:
+                    System.out.println("Opcao invalida. Tente novamente" ) ;
             }
 
-        }while(opcao != 7);
+        }while(opcao != 'g');
         scanner.close();
     }
 }
+
